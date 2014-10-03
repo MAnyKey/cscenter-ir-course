45fcacba37b424e5a3fff9dbeb9c788f7e3f9bfb
Maxim Kolganov <manykey@yandex-team.ru>
2014-10-04 01:17:05 +0400

    completed morphology

--- simple-search/HunspellLemmer.hs
+++ simple-search/HunspellLemmer.hs
@@ -1,9 +1,10 @@
 {-# LANGUAGE CPP, ForeignFunctionInterface, EmptyDataDecls, DeriveDataTypeable #-}
-module HunspellLemmer  (Hunspell, makeHunspell, analyzeText, analyze) where
+module HunspellLemmer  (Hunspell, makeHunspell, stemText, stem) where
 
 import Data.Typeable
 import System.IO.Unsafe
 
+import Data.Monoid
 import Control.Monad
 import Control.Exception
 
@@ -11,6 +12,7 @@ import Foreign hiding (unsafePerformIO)
 import Foreign.C.String
 import Foreign.C.Types
 import Foreign.Ptr
+import qualified Foreign.Concurrent as FC
 import Foreign.ForeignPtr
 
 import Data.ByteString (ByteString)
@@ -28,7 +30,7 @@ type CStringList = (Ptr CString)
 
 foreign import ccall "Hunspell_create" c_hunspell_create :: CString -> CString -> IO (Ptr Hunhandle)
 foreign import ccall "&Hunspell_destroy" c_hunspell_destroy :: FunPtr (Ptr Hunhandle -> IO ())
-foreign import ccall "Hunspell_analyze" c_hunspell_analyze :: Ptr Hunhandle -> Ptr CStringList -> CString -> IO CInt
+foreign import ccall "Hunspell_stem" c_hunspell_stem :: Ptr Hunhandle -> Ptr CStringList -> CString -> IO CInt
 foreign import ccall "Hunspell_free_list" c_hunspell_free_list :: Ptr Hunhandle -> Ptr CStringList -> CInt -> IO ()
 
 type FreeList = Ptr (Ptr CString) -> IO ()
@@ -47,36 +49,31 @@ makeHunspell aff_path dic_path =
       hunhandle <- newForeignPtr c_hunspell_destroy hunhandlePtr
       return $ Just (Hunspell hunhandle)
 
-withFreeList f action = bracket (mkFreeList f) freeHaskellFunPtr action
+withStringList handle p_string_list size = bracket (return p_string_list) free
+  where free list = c_hunspell_free_list handle list size
 
-withStringList handle p_string_list size action = do
-  let free list = c_hunspell_free_list handle list size
-  withFreeList free $ \freeList -> do
-    string_list <- newForeignPtr freeList p_string_list
-    withForeignPtr string_list $ \p_string_list -> do
-      peek p_string_list >>= action
+handleEmptyList p_list c_size action = case c_size == 0 of
+  True  -> return mempty
+  False -> peek p_list >>= action
 
-ptrsToByteStrings :: CStringList -> CInt -> IO [ByteString]
-ptrsToByteStrings list c_size =
-  mapM (peekElemOff list >=> B.packCString) . take (fromIntegral c_size) $ [0..]
+strings :: Ptr CStringList -> CInt -> IO [ByteString]
+strings p_list c_size = handleEmptyList p_list c_size $ \list -> do
+    mapM (peekElemOff list >=> B.packCString) . take (fromIntegral c_size) $ [0..]
 
-ptrsToByteStrings' :: CStringList -> CInt -> IO (Vector ByteString)
-ptrsToByteStrings' list =
-  V.mapM (peekElemOff list >=> B.packCString) . V.enumFromN 0 . fromIntegral
+strings' :: Ptr CStringList -> CInt -> IO (Vector ByteString)
+strings' p_list c_size = handleEmptyList p_list c_size $ \list -> do
+  V.mapM (peekElemOff list >=> B.packCString) . V.enumFromN 0 . fromIntegral $ c_size
 
-strings :: Ptr Hunhandle -> Ptr CStringList -> CInt -> IO [ByteString]
-strings handle p_string_list size =
-  withStringList handle p_string_list size $ \string_list -> do
-    ptrsToByteStrings string_list size
 
-{-# NOINLINE analyze #-}
-analyze :: Hunspell -> ByteString -> [ByteString]
-analyze (Hunspell handle) str = unsafePerformIO $ do
+{-# NOINLINE stem #-}
+stem :: Hunspell -> ByteString -> [ByteString]
+stem (Hunspell handle) str = unsafePerformIO $ do
   withForeignPtr handle $ \handle -> do
   B.useAsCString str $ \c_str -> do
   alloca $ \p_string_list -> do
-    size <- c_hunspell_analyze handle p_string_list c_str
-    strings handle p_string_list size
+    size <- c_hunspell_stem handle p_string_list c_str
+    withStringList handle p_string_list size $ \string_list -> do
+      strings string_list size
 
-analyzeText :: Hunspell -> Text -> [Text]
-analyzeText hunspell = map E.decodeUtf8 . analyze hunspell . E.encodeUtf8
+stemText :: Hunspell -> Text -> [Text]
+stemText hunspell = map E.decodeUtf8 . stem hunspell . E.encodeUtf8
