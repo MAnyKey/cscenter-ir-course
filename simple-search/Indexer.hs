05db2714faf5a7d8441de0d258af94da4a6d1b1d
Maxim Kolganov <manykey@yandex-team.ru>
2014-10-18 13:45:39 +0400

    clean mess out

--- simple-search/Indexer.hs
+++ simple-search/Indexer.hs
@@ -46,10 +46,9 @@ import qualified Data.Text.IO as TIO
 
 import Text.HTML.TagSoup
 import Text.HTML.TagSoup.Fast.Utf8Only hiding (parseTags)
---import qualified NLP.Tokenize.Text as Tok --hiding (tokenize)
+--import NLP.Tokenize.Text hiding (tokenize)
 
 import HunspellLemmer
---import qualified Tokenizer as LTok
 import Tokenizer hiding (tokenize)
 
 data PushVector a = PVec {-# UNPACK #-} !Int !(IOVector a)
@@ -71,46 +70,25 @@ newVector :: MV.Unbox a => IO (PushVector a)
 newVector = PVec 0 <$> MV.new initialSize
   where initialSize = 10
 
-myInnerTextTags = mapMaybe maybeTagText
-
-myTextConcat = LT.concat
-
--- --dumtest :: FilePath -> IO [LT.Text] --IO LT.Text
--- dumtest filePath = do
---   tokens <- tokenize . extractPlainText <$> LTIO.readFile filePath
---   --let allTokens = HashSet.fromList $ tokens
---   return tokens
---   where extractPlainText = foo . parseTagsOptions parseOptions
---         -- foo :: [Tag Text] -> LT.Text
---         foo = myTextConcat . myInnerTextTags
---         -- foo :: [Tag LT.Text] -> LT.Text
---         -- foo = innerText
---         -- foo = id
---         -- filterPunctuation = filter (not . LT.all isPunctuation)
-
---tokenize :: LT.Text -> [LT.Text]
+tokenize :: LT.Text -> [LT.Text]
 tokenize = run (whitespace >=> uris >=> punctuation >=> contractions)
 
-preprocess stemmer = map (stemmer . T.toLower) . filterPunctuation . map LT.toStrict . mytok . extractPlainText
+preprocess :: (Text -> [Text]) -> ByteString -> [[Text]]
+preprocess stemmer = map (stemmer . T.toLower) . filterPunctuation . map LT.toStrict . tokenize . extractPlainText
   where extractPlainText = myInnerText . myparse
         myparse = {-# SCC "myparse" #-} parseTagsT
-        mytok = {-# SCC "mytok" #-} tokenize
         filterPunctuation = filter (not . T.all isPunctuation)
         myInnerText = {-# SCC "inner-text" #-} myTextConcat . myInnerTextTags
           where
             myInnerTextTags = mapMaybe maybeTagText
             myTextConcat = LT.fromChunks
 
-
-stemmer hunspell = stemText hunspell
-
-processFile process filePath = do
-  tokenVariants <- process <$> BS.readFile filePath
-  let allTokens = HashSet.fromList $ concat tokenVariants
+extractAllTokens :: Hunspell -> FilePath -> IO (FilePath, HashSet Text)
+extractAllTokens hunspell filePath = do
+  allTokens <- HashSet.fromList . concat . preprocess stemmer <$> BS.readFile filePath
   return (filePath, allTokens)
-
-
-extractAllTokens hunspell = processFile (preprocess (stemmer hunspell))
+  where
+    stemmer = stemText hunspell
 
 getRecursiveContents :: FilePath -> Producer FilePath IO ()
 getRecursiveContents topPath = do
@@ -123,30 +101,15 @@ getRecursiveContents topPath = do
       then getRecursiveContents path
       else yield path
 
---transform :: (Monad m) => (a -> m b) -> Pipe a b m ()
---transform f = await >>= lift . f >>= yield
-
--- tokens :: Hunspell -> Pipe FilePath (Document, HashSet Text) IO ()
--- tokens hunspell = transform (extractAllTokens hunspell)
-
--- x |> f = f x
-
--- tokens' hunspell = \file -> file |> extractAllTokens hunspell |> lift >>= yield
-
 createDefaultHunspell = makeHunspell "./hunspell-dictionaries/ru_RU.aff" "./hunspell-dictionaries/ru_RU.dic"
-
 
 type DocumentId = Int
 type Document = FilePath
+type TokenIndex = Int
 
-type Index' = BasicHashTable Text (PushVector DocumentId)
+type Index' = BasicHashTable Text (PushVector (DocumentId))
 type DocTable = BasicHashTable DocumentId Document
 
--- invert :: (Foldable t, HashTable h) =>
---           IOHashTable h Text [DocumentId] ->
---           DocumentId ->
---           t Text ->
---           IO ()
 invert :: (Foldable t) => Index' -> DocumentId -> t Text -> IO ()
 invert hashtable document = F.mapM_ insert
   where insert token = do
@@ -154,9 +117,6 @@ invert hashtable document = F.mapM_ insert
           pv <- maybe newVector return val
           pv' <- pushBack pv document
           Hash.insert hashtable token pv'
-          -- case val of
-          --   Just docs -> Hash.insert hashtable token (document:docs)
-          --   Nothing -> Hash.insert hashtable token [document]
 
 printPosting (tok, docs) = (putText tok) >> putStr " " >> print docs
 
@@ -227,11 +187,6 @@ dump indexName (docsTable, index) = do
   L.writeFile indexName serialized
 
 main = do
-  -- [fileName] <- getArgs
-  -- x <- dumtest fileName
-  -- -- TIO.writeFile "output" $ x
-  -- -- print (x `deepseq` ())
-  -- LTIO.writeFile "output"  . LT.pack . show $ x
   [dir, indexName] <- getArgs
   Just hunspell <- createDefaultHunspell
   index <- Hash.new
